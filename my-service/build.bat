a8a51869fddaf7c6d967f0bff85586109ed5cd39
Elody <elody.octavie@efrei.net>
2026-04-24 10:12:52 +0200

    correction tests

--- my-service/build.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "C:\Users\e.octavie\OneDrive - ELECTROCLASS\Documents\EFREI\seconde_annee\devops\devops_tps\my-service"
-call gradlew build -x test
-echo.
-echo BUILD COMPLETED
-pause
